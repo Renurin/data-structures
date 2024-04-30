d33b6be5727797e882cc62284d03071247090e35
Renan Camara <rcamaradeoliveirafarias@gmail.com>
2024-04-29 22:30:44 -0300

    some functions
    add and exclude char from arrays

--- Main_c/arrayC.c
+++ Main_c/arrayC.c
@@ -12,3 +12,31 @@ int *createArrayFloat(int size)
     int *array = malloc(sizeof(int) * size);
     return array;
 }
+
+void *addArray(char *pvetchar, int *ptammax, int *pqtde, char include)
+{
+    if (*pqtde == *ptammax)
+    {
+        *pvetchar = realloc(*pvetchar, *ptammax * 2);
+        *ptammax = *ptammax * 2;
+    }
+
+    pvetchar[*pqtde + 1] = include;
+    *pqtde++;
+}
+void *deleteInArray(char *pvetchar, int *ptammax, int *pqtde, char exclude)
+{
+    char *aux = malloc(*ptammax);
+    for (int i = 0; i < *pqtde; i++)
+    {
+        if (pvetchar[i] == exclude)
+        {
+            continue;
+        }
+        aux[i] = pvetchar[i];
+    }
+    *pqtde--;
+    free(pvetchar);
+    pvetchar = aux;
+    free(aux);
+}
